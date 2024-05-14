54a75d5a373453713ba741eafb0a4785052467b8
Piotr P. Karwasz <piotr.github@karwasz.org>
2024-05-14 10:24:42 +0200

    Add GraalVM example

---
+++ log4j-samples-graalvm/src/main/java/org/apache/logging/log4j/samples/graalvm/Main.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.logging.log4j.samples.graalvm;
+
+import java.time.Instant;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    private Main() {}
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Instant.now())
+                .append(" [")
+                .append(Thread.currentThread().getName())
+                .append("] INFO ")
+                .append(Main.class.getName())
+                .append(" - Hello System.err!");
+        System.err.println(sb);
+        logger.info("Hello Log4j!");
+    }
+}
