2d692e89323fffcc2ba0267106b06cb96d0d7128
Piotr P. Karwasz <piotr.github@karwasz.org>
2024-10-10 17:06:10 +0200

    Enable automatic Android tests (#198)
    This PR cleans up the `log4j-samples-android` project and enables a CI workflow to check the compatibility of releases with Android.

--- log4j-samples-android/app/src/main/java/org/apache/logging/log4j/samples/android/MainActivity.java
+++ log4j-samples-android/app/src/main/java/org/apache/logging/log4j/samples/android/MainActivity.java
@@ -16,67 +16,63 @@
  */
 package org.apache.logging.log4j.samples.android;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
-import java.util.Arrays;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int logLevelIdx;
+    private static final Logger logger = LogManager.getLogger(MainActivity.class);
+    static final int[] buttonIds = {
+        R.id.fatalLogBtn, R.id.errorLogBtn, R.id.warnLogBtn, R.id.infoLogBtn, R.id.debugLogBtn, R.id.traceLogBtn
+    };
+    /**
+     * All log levels
+     */
+    private static final List<Level> logLevels = Arrays.asList(
+            Level.OFF, Level.FATAL, Level.ERROR, Level.WARN, Level.INFO, Level.DEBUG, Level.TRACE, Level.ALL);
 
-    //Logger log = LogManager.getLogger(MainActivity.class);
-    Logger log = LogManager.getRootLogger();
+    private int logLevelIdx;
+    private final Map<View, Level> buttonToLevel = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        logger.info("Creating Main Activity...");
         setContentView(R.layout.activity_main);
 
-        /*// Manually read in log4j2.properties
-        LoggerContext context = (LoggerContext) LogManager.getContext(false);
-        try {
-            InputStream inputStream = getAssets().open("log4j2.properties");
-            ConfigurationSource source = new ConfigurationSource(inputStream);
-            context.start(ConfigurationFactory.getInstance().getConfiguration(context, source));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-        /*// Explicitly set the logging level for this logger to INFO
-        //Configurator.setLevel(log, Level.INFO);
-        LoggerConfig loggerConfig = context.getConfiguration().getLoggerConfig(log.getName());
-        loggerConfig.setLevel(Level.INFO);*/
-
-        //
-        // UI bindings
-        //
-        //TextView displaying Log Level
+        // TextView displaying Log Level
         TextView logLevelTxt = findViewById(R.id.logLevelTxt);
-        logLevelTxt.setText(log.getLevel().name());
+        logLevelTxt.setText(logger.getLevel().name());
 
-        //Change log level
+        // Change log level
         Button setLogLevelBtn = findViewById(R.id.setLogLevelBtn);
         setLogLevelBtn.setOnClickListener(v -> {
             AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
             alertDialog.setTitle("Set log level");
 
-            String[] logLevels = new String[]{"OFF", "FATAL", "ERROR", "WARN", "INFO", "DEBUG", "TRACE", "ALL"};
-            logLevelIdx = Arrays.asList(logLevels).indexOf(log.getLevel().name());
+            logLevelIdx = logLevels.indexOf(logger.getLevel());
 
-            alertDialog.setSingleChoiceItems(logLevels, logLevelIdx, (dialog, which) -> logLevelIdx = which);
+            alertDialog.setSingleChoiceItems(
+                    logLevels.stream().map(Level::name).toArray(String[]::new),
+                    logLevelIdx,
+                    (dialog, which) -> logLevelIdx = which);
             alertDialog.setPositiveButton("Select", (dialog, which) -> {
-                Configurator.setLevel(log, Level.valueOf(Arrays.asList(logLevels).get(logLevelIdx)));
-                logLevelTxt.setText(log.getLevel().name());
+                Configurator.setLevel(logger, logLevels.get(logLevelIdx));
+                logLevelTxt.setText(logger.getLevel().name());
                 dialog.dismiss();
             });
             alertDialog.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
@@ -84,23 +80,20 @@ public class MainActivity extends AppCompatActivity {
             alertDialog.show();
         });
 
-        //Log messages
-        Button fatalLogBtn = findViewById(R.id.fatalLogBtn);
-        fatalLogBtn.setOnClickListener(v -> log.fatal("Current log level is: " + log.getLevel().name()));
+        // Log messages
+        for (int i = 0; i < buttonIds.length; i++) {
+            Button button = findViewById(buttonIds[i]);
+            // The `OFF` level should not have a button
+            buttonToLevel.put(button, logLevels.get(i + 1));
+            button.setOnClickListener(this::onLevelButtonClick);
+        }
+        logger.info("Main Activity created.");
+    }
 
-        Button errorLogBtn = findViewById(R.id.errorLogBtn);
-        errorLogBtn.setOnClickListener(v -> log.error("Current log level is: " + log.getLevel().name()));
-
-        Button warnLogBtn = findViewById(R.id.warnLogBtn);
-        warnLogBtn.setOnClickListener(v -> log.warn("Current log level is: " + log.getLevel().name()));
-
-        Button infoLogBtn = findViewById(R.id.infoLogBtn);
-        infoLogBtn.setOnClickListener(v -> log.info("Current log level is: " + log.getLevel().name()));
-
-        Button debugLogBtn = findViewById(R.id.debugLogBtn);
-        debugLogBtn.setOnClickListener(v -> log.debug("Current log level is: " + log.getLevel().name()));
-
-        Button traceLogBtn = findViewById(R.id.traceLogBtn);
-        traceLogBtn.setOnClickListener(v -> log.trace("Current log level is: " + log.getLevel().name()));
+    void onLevelButtonClick(View view) {
+        Level level = buttonToLevel.get(view);
+        if (level != null) {
+            logger.log(level, "Hello {}!", level);
+        }
     }
 }
